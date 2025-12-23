f63945df020f737a3e905d720b4fe3e2fff47410
Aleksander Piotrowski <pelotasplus@gmail.com>
2025-12-23 23:00:39 +0100

    Initialize Koin DI in Web target and update JS dependencies
    - Call `initKoin()` in `main.kt` for the Web target.
    - Upgrade `kotlin-web-helpers` to 3.0.0.
    - Upgrade `mocha` to 11.7.2.
    - Upgrade `webpack` to 5.101.3.
    - Update `karma` to use the GitHub-hosted version 6.4.5.

--- composeApp/src/webMain/kotlin/pl/pelotasplus/eyeofbeholder/main.kt
+++ composeApp/src/webMain/kotlin/pl/pelotasplus/eyeofbeholder/main.kt
@@ -2,9 +2,12 @@ package pl.pelotasplus.eyeofbeholder
 
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.window.ComposeViewport
+import org.koin.core.context.startKoin
+import pl.pelotasplus.eyeofbeholder.di.initKoin
 
 @OptIn(ExperimentalComposeUiApi::class)
 fun main() {
+    initKoin()
     ComposeViewport {
         App()
     }
